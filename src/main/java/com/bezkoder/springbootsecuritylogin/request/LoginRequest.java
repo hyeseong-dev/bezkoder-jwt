8acae2593a5376b403af23b94b443b47f5590757
HYE SEONG LEE <hyeseong @dtto9.com>
2024-01-26 13:42:47 +0900

    feat. update WebSecurityConfig and LoginRequest

--- src/main/java/com/bezkoder/springbootsecuritylogin/request/LoginRequest.java
+++ src/main/java/com/bezkoder/springbootsecuritylogin/request/LoginRequest.java
@@ -11,7 +11,7 @@ public class LoginRequest {
 
     @NotBlank(message = "Password is required")
     @Size(min = 8, message = "Password must be at least 8 characters")
-    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$",
+    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[^A-Za-z\\d])[A-Za-z\\d[^A-Za-z\\d]]{8,}$",
             message = "Password must contain at least one uppercase letter, one lowercase letter, one number and one special character")
     private String password;
 
